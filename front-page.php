214841fc403bd93bb392da9bafbba0410c258bc5
nonnavolodina <volodina.nonna@gmail.com>
2021-05-21 15:19:41 -0400

    basic coding of information section on homepage

--- front-page.php
+++ front-page.php
@@ -1,2 +1,30 @@
 <?php get_header(); ?>
+<main class="homepage">
+    <section class="hero">
+        <div class="wrapper">
+            <p class="hero__tagline">Learn with us</p>
+            <h1 class="hero__heading">Easy, low prep activities for early childhood development.</h1>
+            <button class="btn btn--fill">Join us</button>
+        </div>
+    </section>
+    <section class="information">
+        <h2 class="information__heading">Playful learning with purpose</h2>
+        <p class="information__description">All of our activities are designed to build children’s foundational literacy and numeracy knowledge and develop motor, social-emotional and creative thinking skills. Activities require little preparation and use everyday items making them easy for busy parents and caregivers to get kids engaged and learning. Each activity is developmentally appropriate for a range of ages 6m to 5 years.</p>
+        <div class="information__details">
+            <div class="information__detail">
+                <span class="blue-circle"></span>
+                <p class="tagline">ZERO OR LOW PREP</p>
+            </div>
+            <div class="information__detail">
+                <span class="blue-circle"></span>
+                <p class="tagline">AGES 6M - 5Y</p>
+            </div>
+            <div class="information__detail">
+                <span class="blue-circle"></span>
+                <p class="tagline">CURRICULUM BASED</p>
+            </div>
+        </div>
+        <button class="btn btn--fill">Join us</button>
+    </section>
+</main>
 <?php get_footer(); ?>
