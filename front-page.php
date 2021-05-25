1325ee40f91a227bcff55d2859173ded4f06236f
nonnavolodina <volodina.nonna@gmail.com>
2021-05-25 13:12:39 -0400

    Added basic structure of curriculum section on homepage

--- front-page.php
+++ front-page.php
@@ -26,5 +26,33 @@
         </div>
         <button class="btn btn--fill">Join us</button>
     </section>
+    <section class="wwc" style="background-image:url('<?php bloginfo('template_url')?>/images/wwc-background.png');">
+        <p class="wwc__tagline">WHAT WE COVER</p>
+        <div class="wwc__content wrapper">
+            <p>Literacy</p>
+            <p>Numeracy</p>
+            <p>Art</p>
+            <p>Motor</p>
+            <p>Social + Emotional</p>
+        </div>
+    </section>
+    <section class="curriculum wrapper">
+        <div class="curriculum__container">
+            <div class="curriculum__inner-container">
+                <h2 class="curriculum__heading">Our curriculum</h2>
+                <p class="curriculum__description">Yes, children need to develop the pre-academic skills that will make them readers, writers and strong communicators (literacy). They also need to build a foundation that enables them to solve math problems, conduct experiments, and understand time, money and shapes (numeracy).</p>
+                <p class="curriculum__description">But successful learners need to master more than content. A child’s motor (eg. physical development), social-emotional (eg. self-regulation, empathy and theory of mind), and creative (eg. imagination and self expression) development is of equal, and at times greater, importance.</p>
+                <p class="curriculum__description">When children develop the ability to explore their environment, be resourceful about the materials that they engage with, and think flexibly, they are better equipped to tackle any new content they’re exposed to.</p>
+            </div>
+            <img class="curriculum__img" src="<?php bloginfo('template_url')?>/images/boy_learning.png" alt="">
+        </div>
+        <div class="curriculum__container">
+            <img class="curriculum__img" src="<?php bloginfo('template_url')?>/images/woman.png" alt="">
+            <div class="curriculum__inner-container">
+                <h2 class="curriculum__heading">Expertly designed</h2>
+                <p class="curriculum__description">Our activities are designed by Renee, a teacher, curriculum designer and mother of two. She has degrees in education, child development and instructional design from the University of Victoria and Columbia University. She has worked with children and families in Canada and the United States to conduct learning assessments, devise learning plans and create resources to target key skills. </p>
+            </div>
+        </div>
+    </section>
 </main>
 <?php get_footer(); ?>
