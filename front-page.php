08fb4d13d28667be142fda0ba9fd24388f9a7a28
nonnavolodina <volodina.nonna@gmail.com>
2021-05-25 15:12:20 -0400

    basic structure of sign up section on homepage

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
         </div>
     </section>
     <section class="information">
-        <h2 class="information__heading">Playful learning with purpose</h2>
+        <h2 class="h2 h2--medium information__heading">Playful learning with purpose</h2>
         <p class="information__description">All of our activities are designed to build children’s foundational literacy and numeracy knowledge and develop motor, social-emotional and creative thinking skills. Activities require little preparation and use everyday items making them easy for busy parents and caregivers to get kids engaged and learning. Each activity is developmentally appropriate for a range of ages 6m to 5 years.</p>
         <div class="information__details">
             <div class="information__detail">
@@ -39,7 +39,7 @@
     <section class="template-lr wrapper">
         <div class="template-lr__container">
             <div class="template-lr__inner-container">
-                <h2 class="template-lr__heading">Our curriculum</h2>
+                <h2 class="h2 h2--medium template-lr__heading">Our curriculum</h2>
                 <p class="template-lr__description">Yes, children need to develop the pre-academic skills that will make them readers, writers and strong communicators (literacy). They also need to build a foundation that enables them to solve math problems, conduct experiments, and understand time, money and shapes (numeracy).</p>
                 <p class="template-lr__description">But successful learners need to master more than content. A child’s motor (eg. physical development), social-emotional (eg. self-regulation, empathy and theory of mind), and creative (eg. imagination and self expression) development is of equal, and at times greater, importance.</p>
                 <p class="template-lr__description">When children develop the ability to explore their environment, be resourceful about the materials that they engage with, and think flexibly, they are better equipped to tackle any new content they’re exposed to.</p>
@@ -49,7 +49,7 @@
         <div class="template-lr__container">
             <img class="template-lr__img" src="<?php bloginfo('template_url')?>/images/woman.png" alt="">
             <div class="template-lr__inner-container">
-                <h2 class="template-lr__heading">Expertly designed</h2>
+                <h2 class="h2 h2--medium template-lr__heading">Expertly designed</h2>
                 <p class="template-lr__description">Our activities are designed by Renee, a teacher, curriculum designer and mother of two. She has degrees in education, child development and instructional design from the University of Victoria and Columbia University. She has worked with children and families in Canada and the United States to conduct learning assessments, devise learning plans and create resources to target key skills. </p>
             </div>
         </div>
@@ -58,7 +58,7 @@
         <div class="learning-platform__container inner-wrapper">
             <img src="<?php bloginfo('template_url')?>/images/placeholder.png" alt="" class="learning-platform__img">
             <div class="learning-platform__content">
-                <h2 class="learning-platform__heading">The learning platform</h2>
+                <h2 class="h2 h2--medium learning-platform__heading">The learning platform</h2>
                 <p class="learning-platform__description">We have designed an online space for you to easily find activities to engage your child. Filter resource by subject, skills, and ages. Save your favourite activities. Engaging in activities again and again will build your child’s ability to attend to tasks for longer and longer intervals. You’ll also be surprised at how their engagement with that activity changes over time.</p>
             </div>
         </div>
@@ -66,7 +66,7 @@
     <section class="template-lr wrapper">
         <div class="template-lr__container">
             <div class="template-lr__inner-container">
-                <h2 class="template-lr__heading">Play is learning</h2>
+                <h2 class="h2 h2--medium template-lr__heading">Play is learning</h2>
                 <p class="template-lr__description">The deepest learning happens through joyful, engaging, meaningful, and interactive experiences. For young children purposeful play provides these rich, engrossing experiences.</p>
                 <p class="template-lr__description">Learning through play supports the acquisition of both content and higher order thinking skills, as well as healthy physical and social-emotional development.</p>
                 <p class="template-lr__description">Play is the child’s lab. It becomes the space for children to experiment with new knowledge and theories, and reenact experiences to solidify understanding.</p>
@@ -76,11 +76,42 @@
         <div class="template-lr__container">
             <img class="template-lr__img" src="<?php bloginfo('template_url')?>/images/bubbles.png" alt="">
             <div class="template-lr__inner-container">
-                <h2 class="template-lr__heading">Does age matter?</h2>
+                <h2 class="h2 h2--medium template-lr__heading">Does age matter?</h2>
                 <p class="template-lr__description">Our activities are exploratory, where the process is more important than the product. This makes each activity accessible to a wide range of ages. What will vary is the level of adult guidance needed. The older the child the more independent they will be with a given task.</p>
                 <p class="template-lr__description">When picking an activity, their interests matters more than their age. Let the child lead. If they take the art materials and go in a completely different direction with them, celebrate their divergent thinking and creativity. If they ask for literacy activity after literacy activity with little interest in anything that resembles math, go with it. Follow their natural inclinations and dive deeply into their passions. Child-led learning is powerful.</p>
             </div>
         </div>
+    </section>
+    <section class="sign-up">
+        <h2 class="h2 h2--large sign-up__heading">Sign up for Earlybird</h2>
+        <div class="sign-up__content wrapper">
+            <div class="sign-up__rates">
+                <div class="sign-up__rate-cards">
+                    <div class="sign-up__rate-card sign-up__rate-card--extra">
+                        <p class="sign-up__rate-card-heading">Annual</p>
+                        <p class="sign-up__rate-card-price">$72</p>
+                        <small class="sign-up__rate-card-price-desc">$6 USD / month</small>
+                        <button class="btn btn--fill">Join us</button>
+                    </div>
+                    <div class="sign-up__rate-card">
+                        <p class="sign-up__rate-card-heading">Monthly</p>
+                        <p class="sign-up__rate-card-price">$8</p>
+                        <small class="sign-up__rate-card-price-desc">USD / month</small>
+                        <button class="btn btn--fill">Join us</button>
+                    </div>
+                </div>
+            </div>
+            <div class="sign-up__info">
+                <p class="sign-up__info-heading">Get unlimited access to:</p>
+                <ul class="sign-up__info-list">
+                    <li>Over 100 low prep video and printable activities.</li>
+                    <li>1-2 new activities uploaded each week.</li>
+                    <li>Members only newsletter with tips, resources, and bonus activities.</li>
+                    <li>Livestream activities with Renee.</li>
+                </ul>
+            </div>
+        </div>
+        <p class="sign-up__disclaimer">Can't afford a subscription? <a href="/contact">Contact us.</a></p>
     </section>
 </main>
 <?php get_footer(); ?>
