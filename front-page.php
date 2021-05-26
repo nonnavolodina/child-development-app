3bc537996a0e0acb5ba8628818485f660cb49ee7
nonnavolodina <volodina.nonna@gmail.com>
2021-05-26 15:19:52 -0400

    made sign up section on homepage dynamic with ACFs

--- front-page.php
+++ front-page.php
@@ -123,35 +123,54 @@
     </section>
 
     <section class="sign-up">
-        <h2 class="h2 h2--large sign-up__heading">Sign up for Earlybird</h2>
-        <div class="sign-up__content wrapper">
-            <div class="sign-up__rates">
-                <div class="sign-up__rate-cards">
-                    <div class="sign-up__rate-card sign-up__rate-card--extra">
-                        <p class="sign-up__rate-card-heading">Annual</p>
-                        <p class="sign-up__rate-card-price">$72</p>
-                        <small class="sign-up__rate-card-price-desc">$6 USD / month</small>
-                        <button class="btn btn--fill">Join us</button>
+        <?php if(have_rows('sign_up_section')):
+            while (have_rows('sign_up_section')) : the_row();
+                $heading = get_sub_field('heading'); 
+                $disclaimer = get_sub_field('disclaimer'); ?>
+                <h2 class="h2 h2--large sign-up__heading"><?php echo $heading ?></h2> 
+                <div class="sign-up__content wrapper">
+                    <div class="sign-up__rates">
+                        <div class="sign-up__rate-cards">
+                            <?php if(have_rows('rate_cards')):
+                                while (have_rows('rate_cards')) : the_row();
+                                    $rate_heading = get_sub_field('heading');
+                                    $price = get_sub_field('price'); 
+                                    $price_description = get_sub_field('price_description'); 
+                                    $CTA = get_sub_field('cta'); 
+                                    $discount = get_sub_field('discount'); ?>
+                                    <div class="sign-up__rate-card">
+                                        <?php if($discount != null): ?>
+                                            <span class="sign-up__rate-card-discount"><?php echo $discount ?></span>
+                                        <?php endif; ?>
+                                        <p class="sign-up__rate-card-heading"><?php echo $rate_heading ?></p>
+                                        <p class="sign-up__rate-card-price">$<?php echo $price ?></p>
+                                        <small class="sign-up__rate-card-price-desc"><?php echo $price_description ?></small>
+                                        <button class="btn btn--fill"><?php echo $CTA ?></button>
+                                    </div>
+                                <?php endwhile; ?>
+                            <?php endif; ?>    
+                        </div>
                     </div>
-                    <div class="sign-up__rate-card">
-                        <p class="sign-up__rate-card-heading">Monthly</p>
-                        <p class="sign-up__rate-card-price">$8</p>
-                        <small class="sign-up__rate-card-price-desc">USD / month</small>
-                        <button class="btn btn--fill">Join us</button>
+                    <div class="sign-up__info">
+                        <?php if(have_rows('information')):
+                            while (have_rows('information')) : the_row();
+                                $heading = get_sub_field('heading'); ?>
+                                <p class="sign-up__info-heading"><?php echo $heading ?></p>  
+                                <ul class="sign-up__info-list">
+                                    <?php if(have_rows('info_list')):
+                                        while (have_rows('info_list')) : the_row();
+                                            $info_list_item = get_sub_field('info_list_item'); ?>
+                                            <li><?php echo $info_list_item ?></li>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>   
+                                </ul>
+                            <?php endwhile; ?>
+                        <?php endif; ?> 
                     </div>
                 </div>
-            </div>
-            <div class="sign-up__info">
-                <p class="sign-up__info-heading">Get unlimited access to:</p>
-                <ul class="sign-up__info-list">
-                    <li>Over 100 low prep video and printable activities.</li>
-                    <li>1-2 new activities uploaded each week.</li>
-                    <li>Members only newsletter with tips, resources, and bonus activities.</li>
-                    <li>Livestream activities with Renee.</li>
-                </ul>
-            </div>
-        </div>
-        <p class="sign-up__disclaimer">Can't afford a subscription? <a href="/contact">Contact us.</a></p>
+                <div class="sign-up__disclaimer"><?php echo $disclaimer ?></div>
+            <?php endwhile; ?>
+        <?php endif; ?>   
     </section>
     <?php get_template_part('templates/instagram'); ?>
 </main>
