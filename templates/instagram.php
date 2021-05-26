afae3e0d63b3cf430ddc5f6cb3348d2de016fc7a
nonnavolodina <volodina.nonna@gmail.com>
2021-05-26 15:39:38 -0400

    updated class name from tabline to subheading

--- templates/instagram.php
+++ templates/instagram.php
@@ -1,5 +1,5 @@
 <section class="instagram">
-    <p class="instagram__tagline">follow us</p>
+    <p class="instagram__subheading">follow us</p>
     <h2 class="h2 h2--medium instagram__heading">@learnwithearlybird</h2>
     <div class="instagram__posts"></div>
 </section>
