de8b99cd49d3aa53f6768b3a7c23195dfdc2cadc
nonnavolodina <volodina.nonna@gmail.com>
2021-05-28 16:12:22 -0400

    Made Activities section on logged in homepage responsive

--- front-page.php
+++ front-page.php
@@ -1,32 +1,34 @@
 <?php get_header(); ?>
 <main class="homepage">
-    <?php if(have_rows('hero_slider')):
-        while (have_rows('hero_slider')) : the_row();
-            $background_image = get_sub_field('background_image');
-            $background_colour = get_sub_field('background_colour');
-            $subheading = get_sub_field('subheading');
-            $heading = get_sub_field('heading');
-            $CTA = get_sub_field('cta');
-            $is_slider = get_row_index();
-            if($is_slider >= 1): ?>
-                <section class="hero hero--slider" style="<?php if(!empty($background_image)): ?>background-image: url('<?php echo $background_image ?>') <?php else: ?>background-color: <?php echo $background_colour?> <?php endif; ?>;">
-                    <div class="wrapper">
-                        <p class="hero__subheading"><?php echo $subheading ?></p>
-                        <h1 class="hero__heading"><?php echo $heading ?></h1>
-                        <button class="btn btn--fill"><?php echo $CTA ?></button>
-                    </div>
-                </section>
-            <?php else: ?>
-                <section class="hero hero--slider" style="<?php if(!empty($background_image)): ?>background-image: url('<?php echo $background_image ?>') <?php else: ?>background-color: <?php echo $background_colour?> <?php endif; ?>;">
-                    <div class="wrapper">
-                        <p class="hero__subheading"><?php echo $subheading ?></p>
-                        <h1 class="hero__heading"><?php echo $heading ?></h1>
-                        <button class="btn btn--fill"><?php echo $CTA ?></button>
-                    </div>
-                </section>
-            <?php endif; ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <?php if ( !is_user_logged_in() ) { ?>
+        <?php if(have_rows('hero_slider')):
+            while (have_rows('hero_slider')) : the_row();
+                $background_image = get_sub_field('background_image');
+                $background_colour = get_sub_field('background_colour');
+                $subheading = get_sub_field('subheading');
+                $heading = get_sub_field('heading');
+                $CTA = get_sub_field('cta');
+                $is_slider = get_row_index();
+                if($is_slider >= 1): ?>
+                    <section class="hero hero--slider" style="<?php if(!empty($background_image)): ?>background-image: url('<?php echo $background_image ?>') <?php else: ?>background-color: <?php echo $background_colour?> <?php endif; ?>;">
+                        <div class="wrapper">
+                            <p class="hero__subheading"><?php echo $subheading ?></p>
+                            <h1 class="hero__heading"><?php echo $heading ?></h1>
+                            <button class="btn btn--fill"><?php echo $CTA ?></button>
+                        </div>
+                    </section>
+                <?php else: ?>
+                    <section class="hero hero--slider" style="<?php if(!empty($background_image)): ?>background-image: url('<?php echo $background_image ?>') <?php else: ?>background-color: <?php echo $background_colour?> <?php endif; ?>;">
+                        <div class="wrapper">
+                            <p class="hero__subheading"><?php echo $subheading ?></p>
+                            <h1 class="hero__heading"><?php echo $heading ?></h1>
+                            <button class="btn btn--fill"><?php echo $CTA ?></button>
+                        </div>
+                    </section>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    <?php } ?>
     
     <?php if ( is_user_logged_in() ) { ?>
         <?php if(have_rows('activities_by_subject')):
@@ -90,67 +92,74 @@
             <?php endwhile; ?>
         <?php endif; ?>
     <?php } ?>
-    
-    <?php if(have_rows('information')):
-        while (have_rows('information')) : the_row();
-            $heading = get_sub_field('heading');
-            $description = get_sub_field('description');
-            $CTA = get_sub_field('cta_text'); ?>
-            <section class="information">
-                <h2 class="h2 h2--medium information__heading"><?php echo $heading ?></h2>
-                <p class="information__description"><?php echo $description ?></p>
-                <div class="information__details">
-                    <?php if(have_rows('information_detail')):
-                        while (have_rows('information_detail')) : the_row();
-                            $detail_tagline = get_sub_field('detail_tagline'); ?>
-                            <div class="information__detail">
-                                <span class="blue-circle"></span>
-                                <p class="subheading"><?php echo $detail_tagline ?></p>
-                            </div>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
-                <button class="btn btn--fill"><?php echo $CTA ?></button>
-            </section>
-        <?php endwhile; ?>
-    <?php endif; ?>
 
-    <?php if(have_rows('what_we_cover')):
-        while (have_rows('what_we_cover')) : the_row();
-            $heading = get_sub_field('heading'); ?>
-            <section class="wwc" style="background-image:url('<?php bloginfo('template_url')?>/images/wwc-background.png');">
-                <p class="wwc__subheading"><?php echo $heading ?></p>
-                <div class="wwc__content wrapper">
-                <?php if(have_rows('covered_subjects')):
-                    while (have_rows('covered_subjects')) : the_row();
-                        $subjects = get_sub_field('subjects'); ?>
-                        <p><?php echo $subjects ?></p>
-                    <?php endwhile; ?>    
-                <?php endif; ?>
-                </div>
-            </section>
-        <?php endwhile; ?>
-    <?php endif; ?>
-
-    <section class="template-lr wrapper">
-        <?php if(have_rows('curriculum_section')):
-            while (have_rows('curriculum_section')) : the_row();
+    <?php if ( !is_user_logged_in() ) { ?>
+        <?php if(have_rows('information')):
+            while (have_rows('information')) : the_row();
                 $heading = get_sub_field('heading');
                 $description = get_sub_field('description');
-                $image = get_sub_field('image');
-                $position = get_sub_field('image_position'); ?>
-                <div class="template-lr__container <?php if($position == 'left'): ?>template-lr__container--reverse <?php endif; ?>">
-                    <div class="template-lr__inner-container">
-                        <h2 class="h2 h2--medium template-lr__heading"><?php echo $heading ?></h2>
-                        <div class="template-lr__description"><?php echo $description ?></div>
+                $CTA = get_sub_field('cta_text'); ?>
+                <section class="information">
+                    <h2 class="h2 h2--medium information__heading"><?php echo $heading ?></h2>
+                    <p class="information__description"><?php echo $description ?></p>
+                    <div class="information__details">
+                        <?php if(have_rows('information_detail')):
+                            while (have_rows('information_detail')) : the_row();
+                                $detail_tagline = get_sub_field('detail_tagline'); ?>
+                                <div class="information__detail">
+                                    <span class="blue-circle"></span>
+                                    <p class="subheading"><?php echo $detail_tagline ?></p>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
                     </div>
-                    <img class="template-lr__img" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_url($image['alt']); ?>">
-                </div>
+                    <button class="btn btn--fill"><?php echo $CTA ?></button>
+                </section>
             <?php endwhile; ?>
         <?php endif; ?>
-    </section>
+    <?php } ?>
     
-    <?php if(have_rows('learning_platform_section')):
+    <?php if ( !is_user_logged_in() ) { ?>
+        <?php if(have_rows('what_we_cover')):
+            while (have_rows('what_we_cover')) : the_row();
+                $heading = get_sub_field('heading'); ?>
+                <section class="wwc" style="background-image:url('<?php bloginfo('template_url')?>/images/wwc-background.png');">
+                    <p class="wwc__subheading"><?php echo $heading ?></p>
+                    <div class="wwc__content wrapper">
+                    <?php if(have_rows('covered_subjects')):
+                        while (have_rows('covered_subjects')) : the_row();
+                            $subjects = get_sub_field('subjects'); ?>
+                            <p><?php echo $subjects ?></p>
+                        <?php endwhile; ?>    
+                    <?php endif; ?>
+                    </div>
+                </section>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    <?php } ?>
+    
+    <?php if ( !is_user_logged_in() ) { ?>
+        <section class="template-lr wrapper">
+            <?php if(have_rows('curriculum_section')):
+                while (have_rows('curriculum_section')) : the_row();
+                    $heading = get_sub_field('heading');
+                    $description = get_sub_field('description');
+                    $image = get_sub_field('image');
+                    $position = get_sub_field('image_position'); ?>
+                    <div class="template-lr__container <?php if($position == 'left'): ?>template-lr__container--reverse <?php endif; ?>">
+                        <div class="template-lr__inner-container">
+                            <h2 class="h2 h2--medium template-lr__heading"><?php echo $heading ?></h2>
+                            <div class="template-lr__description"><?php echo $description ?></div>
+                        </div>
+                        <img class="template-lr__img" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_url($image['alt']); ?>">
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </section>
+    <?php } ?>
+    
+    <?php if ( !is_user_logged_in() ) { ?>
+        <?php if(have_rows('learning_platform_section')):
             while (have_rows('learning_platform_section')) : the_row();
                 $image = get_sub_field('image');
                 $heading = get_sub_field('heading');
@@ -164,77 +173,83 @@
                         </div>
                     </div>
                 </section>
-        <?php endwhile; ?>
-    <?php endif; ?>
-
-    <section class="template-lr wrapper">
-        <?php if(have_rows('learning_section')):
-            while (have_rows('learning_section')) : the_row();
-                $heading = get_sub_field('heading');
-                $description = get_sub_field('description');
-                $image = get_sub_field('image');
-                $position = get_sub_field('image_position'); ?>
-                <div class="template-lr__container <?php if($position == 'left'): ?>template-lr__container--reverse <?php endif; ?>">
-                    <div class="template-lr__inner-container">
-                        <h2 class="h2 h2--medium template-lr__heading"><?php echo $heading ?></h2>
-                        <div class="template-lr__description"><?php echo $description ?></div>
-                    </div>
-                    <img class="template-lr__img" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_url($image['alt']); ?>">
-                </div>
             <?php endwhile; ?>
         <?php endif; ?>
-    </section>
+    <?php } ?>
 
-    <section class="sign-up">
-        <?php if(have_rows('sign_up_section')):
-            while (have_rows('sign_up_section')) : the_row();
-                $heading = get_sub_field('heading'); 
-                $disclaimer = get_sub_field('disclaimer'); ?>
-                <h2 class="h2 h2--large sign-up__heading"><?php echo $heading ?></h2> 
-                <div class="sign-up__content wrapper">
-                    <div class="sign-up__rates">
-                        <div class="sign-up__rate-cards">
-                            <?php if(have_rows('rate_cards')):
-                                while (have_rows('rate_cards')) : the_row();
-                                    $rate_heading = get_sub_field('heading');
-                                    $price = get_sub_field('price'); 
-                                    $price_description = get_sub_field('price_description'); 
-                                    $CTA = get_sub_field('cta'); 
-                                    $discount = get_sub_field('discount'); ?>
-                                    <div class="sign-up__rate-card">
-                                        <?php if($discount != null): ?>
-                                            <span class="sign-up__rate-card-discount"><?php echo $discount ?></span>
-                                        <?php endif; ?>
-                                        <p class="sign-up__rate-card-heading"><?php echo $rate_heading ?></p>
-                                        <p class="sign-up__rate-card-price">$<?php echo $price ?></p>
-                                        <small class="sign-up__rate-card-price-desc"><?php echo $price_description ?></small>
-                                        <button class="btn btn--fill"><?php echo $CTA ?></button>
-                                    </div>
+    <?php if ( !is_user_logged_in() ) { ?>        
+        <section class="template-lr wrapper">
+            <?php if(have_rows('learning_section')):
+                while (have_rows('learning_section')) : the_row();
+                    $heading = get_sub_field('heading');
+                    $description = get_sub_field('description');
+                    $image = get_sub_field('image');
+                    $position = get_sub_field('image_position'); ?>
+                    <div class="template-lr__container <?php if($position == 'left'): ?>template-lr__container--reverse <?php endif; ?>">
+                        <div class="template-lr__inner-container">
+                            <h2 class="h2 h2--medium template-lr__heading"><?php echo $heading ?></h2>
+                            <div class="template-lr__description"><?php echo $description ?></div>
+                        </div>
+                        <img class="template-lr__img" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_url($image['alt']); ?>">
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </section>
+    <?php } ?>
+
+    <?php if ( !is_user_logged_in() ) { ?>
+        <section class="sign-up">
+            <?php if(have_rows('sign_up_section')):
+                while (have_rows('sign_up_section')) : the_row();
+                    $heading = get_sub_field('heading'); 
+                    $disclaimer = get_sub_field('disclaimer'); ?>
+                    <h2 class="h2 h2--large sign-up__heading"><?php echo $heading ?></h2> 
+                    <div class="sign-up__content wrapper">
+                        <div class="sign-up__rates">
+                            <div class="sign-up__rate-cards">
+                                <?php if(have_rows('rate_cards')):
+                                    while (have_rows('rate_cards')) : the_row();
+                                        $rate_heading = get_sub_field('heading');
+                                        $price = get_sub_field('price'); 
+                                        $price_description = get_sub_field('price_description'); 
+                                        $CTA = get_sub_field('cta'); 
+                                        $discount = get_sub_field('discount'); ?>
+                                        <div class="sign-up__rate-card">
+                                            <?php if($discount != null): ?>
+                                                <span class="sign-up__rate-card-discount"><?php echo $discount ?></span>
+                                            <?php endif; ?>
+                                            <p class="sign-up__rate-card-heading"><?php echo $rate_heading ?></p>
+                                            <p class="sign-up__rate-card-price">$<?php echo $price ?></p>
+                                            <small class="sign-up__rate-card-price-desc"><?php echo $price_description ?></small>
+                                            <button class="btn btn--fill"><?php echo $CTA ?></button>
+                                        </div>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>    
+                            </div>
+                        </div>
+                        <div class="sign-up__info">
+                            <?php if(have_rows('information')):
+                                while (have_rows('information')) : the_row();
+                                    $heading = get_sub_field('heading'); ?>
+                                    <p class="sign-up__info-heading"><?php echo $heading ?></p>  
+                                    <ul class="sign-up__info-list">
+                                        <?php if(have_rows('info_list')):
+                                            while (have_rows('info_list')) : the_row();
+                                                $info_list_item = get_sub_field('info_list_item'); ?>
+                                                <li><?php echo $info_list_item ?></li>
+                                            <?php endwhile; ?>
+                                        <?php endif; ?>   
+                                    </ul>
                                 <?php endwhile; ?>
-                            <?php endif; ?>    
+                            <?php endif; ?> 
                         </div>
                     </div>
-                    <div class="sign-up__info">
-                        <?php if(have_rows('information')):
-                            while (have_rows('information')) : the_row();
-                                $heading = get_sub_field('heading'); ?>
-                                <p class="sign-up__info-heading"><?php echo $heading ?></p>  
-                                <ul class="sign-up__info-list">
-                                    <?php if(have_rows('info_list')):
-                                        while (have_rows('info_list')) : the_row();
-                                            $info_list_item = get_sub_field('info_list_item'); ?>
-                                            <li><?php echo $info_list_item ?></li>
-                                        <?php endwhile; ?>
-                                    <?php endif; ?>   
-                                </ul>
-                            <?php endwhile; ?>
-                        <?php endif; ?> 
-                    </div>
-                </div>
-                <div class="sign-up__disclaimer"><?php echo $disclaimer ?></div>
-            <?php endwhile; ?>
-        <?php endif; ?>   
-    </section>
+                    <div class="sign-up__disclaimer"><?php echo $disclaimer ?></div>
+                <?php endwhile; ?>
+            <?php endif; ?>   
+        </section>
+    <?php } ?>
+
     <?php get_template_part('templates/instagram'); ?>
 </main>
 <?php get_footer(); ?>
