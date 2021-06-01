37f4c69a115b83aafe46f0b5305d36aca8bb4283
nonnavolodina <volodina.nonna@gmail.com>
2021-06-01 16:44:24 -0400

    Updated markup and SCSS files to make sidebar responsive

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,12 @@
 <aside class="sidebar">
-    <p class="sidebar__label">Filter</p>
-    <?php dynamic_sidebar('Activities Filter'); ?>
+    <div class="sidebar--desktop">
+        <p class="sidebar__label">Filter</p>
+        <?php dynamic_sidebar('Activities Filter'); ?>
+    </div>
+    <div class="sidebar--mobile">
+        <div class="buttons">
+            <button class="btn btn--outline">Filter</button>
+            <button class="btn btn--outline">Sort</button>
+        </div>
+    </div>
 </aside>
