c2e365fc4717efff273a8b4a24d4e1d971ebe197
nonnavolodina <volodina.nonna@gmail.com>
2021-06-02 11:34:08 -0400

    added permalink to activity

--- all-activities.php
+++ all-activities.php
@@ -25,41 +25,43 @@ get_header();  ?>
             <div class="all-activities__content">
                 <section class="activities">
                     <?php while ( $activities->have_posts() ) : $activities->the_post(); ?>
-                        <article class="activity">
-                            <div class="activity__image">
-                                <?php the_post_thumbnail(''); ?>
-                            </div>
-                            <div class="activity__content">
-                                <h3 class="h3 activity__heading"><?php the_title(); ?></h3>
-                                <div class="activity__description"><?php the_content(); ?></div>
-                                <ul class="skills">
-                                    <?php
-                                        $skills = get_terms( 'skills', array('hide_empty' => true) );
-                                        foreach($skills as $skill) {
-                                            echo '<li class="skills__skill">' . $skill->name . '</li>';
-                                        }
-                                    ?>
-                                </ul>
-                                <ul class="subjects-ages">
-                                    <div class="subjects-ages__subjects">
+                        <a href="<?php the_permalink(); ?>">
+                            <article class="activity">
+                                <div class="activity__image">
+                                    <?php the_post_thumbnail(''); ?>
+                                </div>
+                                <div class="activity__content">
+                                    <h3 class="h3 activity__heading"><?php the_title(); ?></h3>
+                                    <div class="activity__description"><?php the_content(); ?></div>
+                                    <ul class="skills">
                                         <?php
-                                            $subjects = get_terms( 'subject', array('hide_empty' => true) );
-                                            foreach($subjects as $subject) {
-                                                echo '<button class="subjects__subject subjects__subject--' . $subject->slug . '">' . $subject->name . '</button>';
+                                            $skills = get_terms( 'skills', array('hide_empty' => true) );
+                                            foreach($skills as $skill) {
+                                                echo '<li class="skills__skill">' . $skill->name . '</li>';
                                             }
                                         ?>
-                                    </div>
-                                    <div class="subjects-ages__ages">
-                                        <?php
-                                            $ages = get_terms( 'age', array('hide_empty' => true) );
-                                            foreach($ages as $age) {
-                                                echo '<button class="ages__age">' . $age->name . '</button>';
-                                            }
-                                        ?>
-                                    </div>
-                                </ul>
-                            </div>
-                        </article>
+                                    </ul>
+                                    <ul class="subjects-ages">
+                                        <div class="subjects-ages__subjects">
+                                            <?php
+                                                $subjects = get_terms( 'subject', array('hide_empty' => true) );
+                                                foreach($subjects as $subject) {
+                                                    echo '<button class="subjects__subject subjects__subject--' . $subject->slug . '">' . $subject->name . '</button>';
+                                                }
+                                            ?>
+                                        </div>
+                                        <div class="subjects-ages__ages">
+                                            <?php
+                                                $ages = get_terms( 'age', array('hide_empty' => true) );
+                                                foreach($ages as $age) {
+                                                    echo '<button class="ages__age">' . $age->name . '</button>';
+                                                }
+                                            ?>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </article>
+                        </a>
                     <?php endwhile; ?>
                 </section>
             </div>
