355511916498374df7c5798baafeb0fdbbcb16ed
nonnavolodina <volodina.nonna@gmail.com>
2021-06-17 20:33:06 -0400

    updated homepage, typography, and footer styles for mobile

--- js/src/app.js
+++ js/src/app.js
@@ -5,6 +5,26 @@ jQuery(document).ready(function($) {
         speed: 300,
         slidesToShow: 1,
         arrows: true,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    adaptiveHeight: false,
+                    centerMode: true,
+                    centerPadding: '10px'
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    arrows: false,
+                    adaptiveHeight: false,
+                    centerMode: true,
+                    centerPadding: '5px'
+                }
+            }
+        ]
     });
 });
